0855153bbd373c963eb50dd20558737fca914e61
KARAN PRAVEEN <karansiamintern@gmail.com>
2021-04-21 11:37:24 +0530

    Creating new table in database.

--- Creating_table.py
+++ Creating_table.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "password",
+    password = "SriRamJanaki13<3",
     database = "pythonprogramming"
 )
 
@@ -14,7 +14,7 @@ mycursor =mydb.cursor("SHOW DATABASES")
 for x in mycursor:
     print(x)
 
-mycursor.execute("CREATE TABLE Customers (name VARCHAR(255), address VARCHAR(255))
+mycursor.execute("CREATE TABLE Customers (name VARCHAR(255), address VARCHAR(255)")
 
 mycursor.execute("SHOW TABLES")
 
