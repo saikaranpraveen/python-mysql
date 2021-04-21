08961666ada3f6aceacab64091686887bf5b30f7
KARAN PRAVEEN <karansiamintern@gmail.com>
2021-04-21 11:42:33 +0530

    Creating new table in database

--- Creating_table.py
+++ Creating_table.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "SriRamJanaki13<3",
+    password = "password",
     database = "pythonprogramming"
 )
 
